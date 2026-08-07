889ab82c06eb2b97e3be9542b6f59e8172417566
AlejoCastilloB <diegoabc1999@gmail.com>
2026-08-07 12:59:41 +0200

    Add popular exercise keywords to popularExercises.ts

---
+++ lib/popularExercises.ts
@@ -0,0 +1,6 @@
+export const POPULAR_EXERCISE_KEYWORDS = [
+  "sentadilla", "press de banca", "peso muerto", "dominada", "press militar",
+  "remo con barra", "curl de bíceps con barra", "extensión de tríceps",
+  "prensa de piernas", "jalón al pecho", "press inclinado", "zancada",
+  "elevación de cadera", "remo con mancuerna", "elevación lateral",
+];
